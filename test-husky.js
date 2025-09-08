6744fa623d3533bdcf644b9d19a7cb0efa3eab33
vipinyadav01 <vipinyadav9m@gmail.com>
2025-09-08 16:14:39 +0530

    Test Husky pre-commit hook again

--- test-husky.js
+++ test-husky.js
@@ -1,12 +1,18 @@
 // This file has intentionally bad formatting to test Husky
-const   badFormatting    =     {
-    "key1"    :   "value1"     ,
-    "key2"   :      "value2"
+const badFormatting = {
+  key1: "value1",
+  key2: "value2",
 };
 
-function   badlyFormattedFunction(    param1,param2   ){
-    const result=param1+param2;
-    return   result   ;
+function badlyFormattedFunction(param1, param2) {
+  const result = param1 + param2;
+  return result;
 }
 
-console.log(   "Testing Husky pre-commit hooks"    );
+console.log("Testing Husky pre-commit hooks");
+
+// Adding more badly formatted code
+const anotherTest = {
+  test: "value",
+  demo: "working",
+};
